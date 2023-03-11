4a6bfe2636d0a2376069da6f0db736d3d7024a3c
ikbalarg <ikbalarguc2404@gmail.com>
2023-03-11 14:38:51 +0300

    hw1 is finished

--- hw1/my_solution/main.c
+++ hw1/my_solution/main.c
@@ -10,20 +10,26 @@
 int main() {
     int num1;
     int num2;
-    int check=1;
-    while (check!=0){
-        printf("Give first number : \n");
-        scanf("%d",&num1);
-        printf("Give second number : \n");
-        scanf("%d",&num2);
-        printf("First Number : %d \nSecond Number : %d\n",num1,num2);
-        //find_gcd(num1,num2);
-        //add_function(num1,num2);
-        multpily_function(num1,num2);
-        printf("\nIf you want to continue press any numerical key except 0...\n");
-        scanf("%d",&check);
-    }
-    /* Find GCD */
-    //find_gcd(num1,num2);
-    
+    printf("Give first number : \n");
+    scanf("%d",&num1);
+    printf("Give second number : \n");
+    scanf("%d",&num2);
+    printf("First Number : %d \nSecond Number : %d\n",num1,num2);
+    find_gcd(num1,num2);
+    printf("Function that adds two numbers\n");
+    printf("Give first number : \n");
+    scanf("%d",&num1);
+    printf("Give second number : \n");
+    scanf("%d",&num2);
+    add_function(num1,num2);
+    printf("Function multiplying two numbers\n");
+    printf("Give first number : \n");
+    scanf("%d",&num1);
+    printf("Give second number : \n");
+    scanf("%d",&num2);
+    multpily_function(num1,num2);
+    printf("Function that checks the range of the number\n");
+    printf("Give first number : \n");
+    scanf("%d",&num1);
+    between1to10(num1);
 }
--- hw1/my_solution/util.c
+++ hw1/my_solution/util.c
@@ -3,7 +3,7 @@
 void find_gcd(int num1,int num2){
     int remainder,larger_num,smaller_num; /* variables defined*/
     if(num2==num1){
-        printf("The gcd is %d",num1); // check the equality of the numbers
+        printf("The gcd is %d\n",num1); // check the equality of the numbers
     }
     else{ // numbers are not equal
         if (num1>num2){ /* large number and small number determined */
@@ -16,7 +16,7 @@ void find_gcd(int num1,int num2){
         }
         while (1==1){ /*continue the process until break statement*/
             if(larger_num%smaller_num==0){ /*  Check the remainder, if it is equal to zero then this is our gcd count, in this case enter this if block*/
-                printf("The GCD is %d ",smaller_num); //print gcd number
+                printf("The GCD is %d\n ",smaller_num); //print gcd number
                 break;
             }
             else{/* If the remainder is not equal to zero, apply the Euclidean algorithm in this else block*/
@@ -35,30 +35,44 @@ void add_function(int num1,int num2){
 }
 
 void multpily_function(int num1,int num2){
+    printf("First number : %d\n",num1);
+    printf("Second number : %d\n",num2);
     int ones,tens,hundreds,result;
     int devide,check=0;
     result=num2*num1;
     if (num2>num1){
         int memory;
-        printf("Giridi");
         check=1;
         memory=num2;
         num2=num1;
         num1=memory;
     }
-    if(100<num2<1000){
+    if(99<num2 & num2<1000){
         ones=(num2%10);
         hundreds=(num2/100);
-        tens=(num2-(hundreds*100))/10; /* (325-300)/10 ---> onlar basamağına ulaşıldı */
-        printf("\t%6d\n\t%6d\n x\n  ------------\n%\t%6d\n\t%5d\n\t%4d\n +\n -------------\n\t%6d"
+        tens=(num2-(hundreds*100))/10; // (325-300)/10 ---> onlar basamağına ulaşıldı
+        printf("\t%6d\n\t%6d\n    x\n    ----------\n\t  %03d\n\t %03d\n\t%03d\n   +\n    -----------\n\t%3d\n"
                ,num1,num2,(ones*num1),(tens*num1),(hundreds*num1),result);
     }
-    else if(100>num2>9){
-        ones=num2%10;
-        tens=num2/10;
-        printf("%d\n%d\nx\n-------\n",num1,num2);
+    else if (num2 >= 10) {  // If the second number has 2 digits
+        ones = num2 % 10;
+        tens = num2 / 10;
+        printf("\t   %3d\n\t   %3d\n\tx\n\t--------\n\t   %03d\n", num1, num2, num1 * ones);
+        printf("\t  %03d\n", num1 * tens);
+        printf("\t+\n");
+        printf("\t--------\n\t  %4d\n", result);
+    } else {  // If the second number has 1 digit
+        printf("\t%5d\n\t%5d\n\tx\n\t-------\n\t%5d\n", num1, num2, num1 * num2);
     }
-    
 }
-    
-
+void between1to10(int num1){
+    if (num1>10 || num1<1){
+        printf("Invalid input\n");
+    }
+    else if (num1<=5){
+        printf("The integer you entered is less than or equal to 5\n");
+    }
+    else if (num1>5){
+        printf("The integer you entered is greater than 5\n");
+    }
+}
