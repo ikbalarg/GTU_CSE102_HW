0241d3d8e630f70ac16e53f724d219d42d5ee4c9
ikbalarg <ikbalarguc2404@gmail.com>
2023-03-12 14:57:39 +0300

    hw0 is finished

--- hw0/my_solution/util.c
+++ hw0/my_solution/util.c
@@ -6,97 +6,60 @@ void fraction_print(int numerator, int denominator) {
 }  /* end fraction_print */
 
 void fraction_add(int n1, int d1, int n2, int d2, int * n3, int * d3) {
-    *n3 = n1*d2 + n2*d1;
+    *n3 = n1*d2 + n2*d1; //add two fractional numbers
     *d3 = d1*d2;
     fraction_simplify(n3, d3);
 } /* end fraction_add */
 
 void fraction_sub(int n1, int d1, int n2, int d2, int * n3, int * d3) {
-    *n3 = n1*d2 - n2*d1;
+    *n3 = n1*d2 - n2*d1; //subtract two fractional numbers
     *d3 = d1*d2;
     fraction_simplify(n3, d3);
 } /* end fraction_sub */
 
 void fraction_mul(int n1, int d1, int n2, int d2, int * n3, int * d3) {
-    *n3=n1*n2;
+    *n3=n1*n2; //multiply two fractional numbers
     *d3=d1*d2;
     fraction_simplify(n3, d3);
 } /* end fraction_mul */
 
 void fraction_div(int n1, int d1, int n2, int d2, int * n3, int * d3) {
-    *n3=n1*d2;
+    *n3=n1*d2; //divide two fractional numbers
     *d3=d1*n2;
     fraction_simplify(n3, d3);
 } /* end fraction_div */
 
 /* Simplify the given fraction such that they are divided by their GCD */
-void fraction_simplify(int * n, int * d) {
-    if (*n>=0 & *d>=0){
-        int common=1;
-        if (*n==*d){
-            common=*n;
-        }
-        else if (*n>*d){
-            int i=1;
-            while (i<*d+1){
-                if (((*n%i)==0)&((*d%i)==0)){
-                    common=i;
-                }
-                i++;
-            }
-        }
-        else if (*n<*d){
-            int i=1;
-            while (i<*n+1){
-                if (((*n%i)==0)&((*d%i)==0)){
-                    common=i;
-                }
-                i++;
-            }
-        }
-        int n1,d1;
-        n1=*n/common;
-        d1=*d/common;
-        *n=n1;
-        *d=d1;
+void fraction_simplify(int * n, int * d) { /*Use the Euclidean algorithm and find GCD.*/ 
+    int gcd;
+    int remainder,larger_num,smaller_num; /* variables defined*/
+    if(*n==*d){
+        gcd=*n; /*The gcd value is equal to the numerator or denominator if the numbers are equal*/
     }
-    else{
-        if(*n<0){
-            *n=*n*(-1);
+    else{ // numbers are not equal
+        if (*n>*d){ /* large number and small number determined */
+            larger_num=*n;
+            smaller_num=*d;
         }
-        if(*d<0){
-            *d=*d*(-1);
+        else{ /* large number and small number determined */
+            larger_num=*d;
+            smaller_num=*n;
         }
-        if (*n>=0 & *d>=0){
-            int common=1;
-            if (*n==*d){
-                common=*n;
+        while (1==1){ /*continue the process until break statement*/
+            if(larger_num%smaller_num==0){ /*  Check the remainder, if it is equal to zero then this is our gcd count, in this case enter this if block*/
+                gcd=smaller_num;
+                break;
             }
-            else if (*n>*d){
-                int i=1;
-                while (i<*d+1){
-                    if (((*n%i)==0)&((*d%i)==0)){
-                        common=i;
-                    }
-                    i++;
-                }
+            else{/* If the remainder is not equal to zero, apply the Euclidean algorithm in this else block*/
+                remainder=larger_num%smaller_num; /* Calculate the remainder and store on this variable. We will  assign this number to small number */
+                larger_num=smaller_num;/*Assign the small number to the large number because of the euclidean alghoritm*/
+                smaller_num=remainder; /*Assign the remainder value to the small number because of the euclidean alghoritm */
             }
-            else if (*n<*d){
-                int i=1;
-                while (i<*n+1){
-                    if (((*n%i)==0)&((*d%i)==0)){
-                        common=i;
-                    }
-                    i++;
-                }
-            }
-            int n1,d1;
-            n1=*n/common;
-            d1=*d/common;
-            *n=-1*n1;
-            *d=d1;
         }
     }
-            
-    
+    *n=*n/gcd; /*simplify the numerator value with GCD*/
+    *d=*d/gcd; /*simplify the denominator value with GCD*/    
+    if(*n>=0 & *d<0){ /* (a/-b) ----> (-a/b) */
+        *n=*n*-1;
+        *d=*d*-1;}
 } /* end fraction_div */
