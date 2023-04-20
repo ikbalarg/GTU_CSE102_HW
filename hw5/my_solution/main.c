6fb5596f45e25e7ff652d8509abba53960b27adc
ikbalarg <ikbalarguc2404@gmail.com>
2023-04-20 20:36:29 +0300

    the third part of hw5 rewritten

--- hw5/my_solution/main.c
+++ hw5/my_solution/main.c
@@ -2,16 +2,15 @@
 #include<ctype.h>
 
 void part1();
-void part2();
+void part3();
 void draw_board(char arr[][3]);
 void game(char arr[][3]);
 int checkGame(char arr[][3]);
-void move(int *moveArr,int orderOfMoves);
+void move(int *moveArr,int orderOfMoves,int *checkValid);
 void counter(char filename[], char *alphabet, int *letterCounter);
-
 int main() {
     part1();
-    part2();
+    part3();
 }
 void part1() {
     char filename[256];
@@ -46,46 +45,76 @@ void counter(char filename[], char *alphabet, int *letterCounter) {
     }
     fclose(file);
 }
-void part2(){
+void part3(){
     char arr[3][3]={
-        {'-', '-', '-'},
-        {'-', '-', '-'},
-        {'-', '-', '-'}
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
     };
     draw_board(arr);
     game(arr);
+    char choice;
+    while(1){
+        char arr[3][3]={
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+        };
+        printf("Do you want to play again? (Y/N) ");
+        if(scanf(" %c", &choice) != 1 || (choice != 'Y' && choice != 'N')){
+            printf("Invalid input. Please try again...\n");
+            while(getchar() != '\n'); // tampon bölgesini temizle
+            continue;
+        }
+        if(choice=='N'){
+            break;
+        } else {
+            draw_board(arr);
+            game(arr);
+        }
+    } 
 }
 void game(char arr[][3]){
     int moveArr[2]={0,0};
     int counts=1;
     int num;
-    //checkGame(arr)!=1
     while(checkGame(arr)!=1){
         num=counts%2;
-        move(moveArr,num);
+        int checkValidMove;
+        move(moveArr,num,&checkValidMove);
         while(1){
             if(arr[moveArr[0]-1][moveArr[1]-1]=='X'||arr[moveArr[0]-1][moveArr[1]-1]=='O'){
                 printf("Invalid input please try again...\n");
-                move(moveArr,num);
+                move(moveArr,num,&checkValidMove);
             }else{
                 break;
             }
         }
         if(num==1){
             arr[moveArr[0]-1][moveArr[1]-1]='X';
-            counts=counts+1;
+            if(checkValidMove==1){
+                counts=counts+1;
+            } 
         }else if(num==0){
             arr[moveArr[0]-1][moveArr[1]-1]='O';
-            counts=counts+1;
+            if(checkValidMove==1){
+                counts=counts+1;
+            }
         }
 
         draw_board(arr);
         checkGame(arr);
+        if(counts>=10){
+            printf("No winner. The game is tied\n");
+            break;
+        }
     }
-    if(num==0){
-        printf("Player 2 wins!");
-    }else{
-        printf("Player 1 wins!");
+    if(counts<10){
+        if(num==0){
+            printf("Player 2 wins!\n");
+        }else{
+            printf("Player 1 wins!\n");
+        }
     }
 }
 int checkGame(char arr[][3]){
@@ -109,7 +138,7 @@ int checkGame(char arr[][3]){
         return 0;
     }
 }
-void move(int *moveArr,int orderOfMoves){
+void move(int *moveArr,int orderOfMoves,int *checkValid){
     int row,col;
     if(orderOfMoves==1){
         printf("Player 1 (X), enter your move (row/col):");
@@ -120,12 +149,15 @@ void move(int *moveArr,int orderOfMoves){
         char buffer;
         while (scanf("%d%d", &row, &col) != 2) { // İki tane integer girilene kadar döngü devam eder
             printf("Invalid input. Please enter two integers separated by a space.\n");
+            *checkValid=0;
             while ((buffer = getchar()) != '\n' && buffer != EOF) {} // Buffer temizle
         }
         if(row>3||row<1||col>3||col<1){
             printf("Invalid input. Please enter two integers separated by a space.\n");
+            *checkValid=0;
             continue;
         }else{
+            *checkValid=1;
             break;
         }
     }
