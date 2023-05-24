4162ae2f171a15f2ec295507d0fbd4c66cd6f334
ikbalarg <ikbalarguc2404@gmail.com>
2023-05-24 14:41:41 +0300

    hw8 finished.

--- hw8/main.c
+++ hw8/main.c
@@ -5,46 +5,57 @@
 
 typedef struct {
     char board[3][3];
+    int number;
+    int direction;
+    int hamle;
 } board;
 
 void printBoard(board *board) {
-    FILE *file = fopen("board.txt", "a");  // Dosyayı "a" (append) modunda aç
+    FILE *file = fopen("board.txt", "a");  // Open file in "a" (append) mode
     
     if (file == NULL) {
-        printf("Dosya açma hatası!\n");
+        printf("File opening error!\n");
         return;
     }
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < 3; i++) { //print lines
+        for (int j = 0; j < 3; j++) { //print columns
             printf("%c ", board->board[i][j]);
-            fprintf(file, "%c ", board->board[i][j]);  // Dosyaya yaz
+            fprintf(file, "%c ", board->board[i][j]); //write to file
         }
         printf("\n");
-        fprintf(file, "\n");  // Dosyaya yeni satır ekle
+        fprintf(file, "\n"); 
     }
     printf("\n");
-    fprintf(file, "\n");  // Dosyaya boş bir satır ekle
+    fprintf(file, "\n"); 
 
-    fclose(file);  // Dosyayı kapat
+    fclose(file);  // close file
 }
-
-void shuffleBoard(board *board) {
+int isValidInput(int number, char dash, char direction) {//returns 1 if it is a valid move, returns 0 if it is an erroneous move
+    if (number >= 1 && number <= 8) {
+        if (dash == '-') {
+            if (direction == 'L' || direction == 'R' || direction == 'U' || direction == 'D') {
+                return 1; // Valid inpput
+            }
+        }
+    }
+    return 0; // Invalid input
+}
+void shuffleBoard(board *board) {//this function creates the board and then mixes this board.
     // Başlangıç durumu
-    char initialBoard[3][3] = {
+    char initialBoard[3][3] = { //create a solved board
         {'1', '2', '3'},
         {'4', '5', '6'},
         {'7', '8', '_'}
     };
-    
-    // Başlangıç durumu tahtaya kopyalanır
+    //the created board is copied to the board struct
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             board->board[i][j] = initialBoard[i][j];
         }
     }
     
-    // Tahtayı rastgele karıştırma
+    // Randomly shuffling the board
     srand(time(NULL));
     
     for (int i = 2; i >= 0; i--) {
@@ -58,80 +69,7 @@ void shuffleBoard(board *board) {
         }
     }
 }
-int isValidInput(int number, char dash, char direction) {
-    if (number >= 1 && number <= 8) {
-        if (dash == '-') {
-            if (direction == 'L' || direction == 'R' || direction == 'U' || direction == 'D') {
-                return 1; // Valid inpput
-            }
-        }
-    }
-    return 0; // Invalid input
-}
-void makeMove(board *board1, int number, char direction) {
-    int i, j;
-    int found = 0;
-    char memory;
-    printf("makemove\n");
-    // İstenen sayının konumunu bulma
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            int ch = board1->board[i][j] - '0';
-            if (ch== number) { // board1 kullanılmalı
-                found = 1;
-                break;
-            }
-        }
-        if (found) {
-            break;
-        }
-    }
-    printf("i %d j %d\n",i,j);
-    // İstenen yöne göre hareket ettirme
-    if (direction == 'R') {
-        if (j < 2) { // board1 kullanılmalı
-            memory=board1->board[i][j+1];
-            board1->board[i][j+1] = number+'0'; // board
-            board1->board[i][j] = memory; // board1 kullanılmalı
-        } else {
-            printf("Hatalı hareket! Lütfen geçerli bir hareket yapın.\n");
-            return;
-        }
-    } else if (direction == 'L') {
-        if (j > 0) { // board1 kullanılmalı
-            memory=board1->board[i][j-1];
-            board1->board[i][j - 1] = number+'0'; // board1 kullanılmalı
-            board1->board[i][j] = memory; // board1 kullanılmalı
-        } else {
-            printf("Hatalı hareket! Lütfen geçerli bir hareket yapın.\n");
-            return;
-        }
-    } else if (direction == 'U') {
-        printf("u\n");
-        if (i > 0) { // board1 kullanılmalı
-            memory=board1->board[i-1][j];
-            board1->board[i - 1][j] = number+'0'; // board1 kullanılmalı
-            board1->board[i][j] = memory; // board1 kullanılmalı
-        } else {
-            printf("Hatalı hareket! Lütfen geçerli bir hareket yapın.\n");
-            return;
-        }
-    } else if (direction == 'D') {
-        if (i < 2) { // board1 kullanılmalı
-            memory=board1->board[i + 1][j];
-            board1->board[i + 1][j] = number+'0'; // board1 kullanılmalı
-            board1->board[i][j] = memory; // board1 kullanılmalı
-        } else {
-            printf("Hatalı hareket! Lütfen geçerli bir hareket yapın.\n");
-            return;
-        }
-    } else {
-        printf("Hatalı hareket! Lütfen geçerli bir hareket yapın.\n");
-        return;
-    }
-    return;
-}
-int checkGame(board *board1){
+int checkGame(board *board1){ //this function checks if the game is over. If the game is over it returns 1, otherwise it returns 0.
     char a00,a01,a02,a10,a11,a12,a20,a21,a22;
     a00=board1->board[0][0];
     a01=board1->board[0][1];
@@ -148,23 +86,207 @@ int checkGame(board *board1){
         return 0;
     }
 }
-void playGameWithUser() {
-    board board1;
+int makeMove(board *board1, int number, char direction) { //this function is used when the board is solved by the user.allows movement on the board
+    int i, j;
+    int found = 0;
+    char memory;
+    // Find the position of the desired number
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            int ch = board1->board[i][j] - '0';
+            if (ch== number) { 
+                found = 1;
+                break;
+            }
+        }
+        if (found) {
+            break;
+        }
+    }
+    // Move according to the desired direction
+    if (direction == 'R') {
+        if (j < 2) { //check the right side
+            memory=board1->board[i][j+1];
+            board1->board[i][j+1] = number+'0';
+            board1->board[i][j] = memory; 
+        } else {
+            printf("Invalid input please try again\n");
+            return 0;
+        }
+    } else if (direction == 'L') {
+        if (j > 0) { //  //check the left side
+            memory=board1->board[i][j-1];
+            board1->board[i][j - 1] = number+'0'; 
+            board1->board[i][j] = memory; 
+        } else {
+            printf("Invalid input please try again\n");
+            return 0;
+        }
+    } else if (direction == 'U') {
+        if (i > 0) { //check the top side
+            memory=board1->board[i-1][j];
+            board1->board[i - 1][j] = number+'0';
+            board1->board[i][j] = memory; 
+        } else {
+            printf("Invalid input please try again\n");
+            return 0;
+        }
+    } else if (direction == 'D') {
+        if (i < 2) { //check the underside
+            memory=board1->board[i + 1][j];
+            board1->board[i + 1][j] = number+'0'; 
+            board1->board[i][j] = memory; 
+        } else {
+            printf("Invalid input please try again\n");
+            return 0;
+        }
+    } else {
+        printf("Invalid input please try again\n");
+        return 0;
+    }
+    return 1;
+}
+int findNumberBoard(board *board1,char direction,int number){//this function is used when solving the board computer.
+    //The computer finds the position of the randomly selected number in the board array and then checks the validity of the desired position. If valid, 1 is returned.
+    int i, j;
+    int found = 0;
+    for (i = 0; i < 3; i++) { //find number
+        for (j = 0; j < 3; j++) {
+            int ch = board1->board[i][j] - '0';
+            if (ch== number) { 
+                found = 1;
+                break;
+            }
+        }
+        if (found) {
+            break;
+        }
+    }
+
+    if(direction=='R'){ //check the right side 
+        if(j==2){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+    else if(direction=='L'){ //check the left side 
+        if(j==0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+    else if(direction=='U'){ //check the top side
+        if(i==0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+    else if(direction=='D'){ //check the underside
+        if(i==2){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+    return 2;
+}
+void autoFinish(board *board1) { //this function decodes the board to the computer.
+    if (checkGame(board1) == 1) { //terminate the function if the board is solved
+        printf("The board is solved!\n");
+        printBoard(board1);
+        return;
+    }
+    struct Combination {
+        int number;
+        char direction;
+    }; //created a structer for all possible combinations on the board
+    struct Combination combinations[] = {
+        {1, 'L'},
+        {1, 'R'},
+        {1, 'U'},
+        {1, 'D'},
+        {2, 'L'},
+        {2, 'R'},
+        {2, 'U'},
+        {2, 'D'},
+        {3, 'L'},
+        {3, 'R'},
+        {3, 'U'},
+        {3, 'D'}
+    };
+    int numCombinations = sizeof(combinations) / sizeof(combinations[0]);
+    // Select a random combination
+    int randomIndex = rand() % numCombinations;
+    int number = combinations[randomIndex].number;
+    char direction = combinations[randomIndex].direction;
+    // Apply the selected combination on the board
+    if (makeMove(board1, number, direction) == 1) {
+        printf("Computer move: %d-%c\n", number, direction);
+        printBoard(board1);
+        board1->hamle=board1->hamle+1;
+        //printf("Hamle: %d",board1->hamle);
+    }
+    
+    autoFinish(board1);
+}
+void write_best_score(int score) {
+    FILE *file = fopen("best_score.txt", "r+");
+    if (file == NULL) {
+        printf("An error occurred while opening the file.\n");
+        return;
+    }
+    if(feof(file)){
+    	fprintf(file,"%d",score);
+    }
+    else{
+    	int current_score;
+    	fscanf(file, "%d", &current_score);
+
+    	if (score > current_score) {
+       	 rewind(file);  // Back to the beginning of the file
+
+       	 fprintf(file, "%d", score);  // write score to file
+	
+            // Delete the rest of the file
+       	 int c;
+        	while ((c = fgetc(file)) != EOF) {
+         	   fputc(' ', file);
+       	 }
+   	 }
+    }
+    fclose(file);
+}
+void print_best_score() {
+    FILE *file = fopen("best_score.txt", "r"); //open the file
+    if (file == NULL) {
+        printf("An error occurred while opening the file.\n");
+        return;
+    }
+
+    int best_score;
+    fscanf(file, "%d", &best_score);  // read the score in the file
+    printf("The best score is %d...\n", best_score);
+
+    fclose(file);
+}
+void playGameWithUser() {//this function makes the user solve the board.
+    board board1; 
     int moves=0,number;
-    shuffleBoard(&board1);
-    printBoard(&board1);
+    shuffleBoard(&board1);//create board 
+    printBoard(&board1); //print the first version of the board
     char direction,dash;
-    int check=0;
-    while(checkGame(&board1)==0){
+    int check=0,validMove;
+    while(checkGame(&board1)==0){ //this block will last until the end of the game.
         int validInput = 0;
-        while (!validInput) {
+        while (!validInput) { //this block continues until correct input is received from the user.
             printf("Enter your move (number-direction,e.g.,2-R): ");
-            int scanned = scanf("%d %c %c", &number, &dash,&direction);
-            //direction = getchar();
-            printf("%c",dash);
+            int scanned = scanf("%d %c %c", &number, &dash,&direction);//receive number, sign and location from the user
             int c;
             while ((c = getchar()) != '\n' && c != EOF);
-            if(dash!='-'){
+            if(dash!='-'){ //if the sign is not a hyphen, suppress the error message and ask again
                 printf("Invalid input. You must put a hyphen between number and position\n");
                 int j;
                 while ((j = getchar()) != '\n' && j != EOF);
@@ -172,17 +294,23 @@ void playGameWithUser() {
             }
             else{
                 if (scanned != 3 || !isValidInput(number, dash, direction)) {
-                    printf("1.Hatalı giriş! Lütfen doğru formatta bir girdi girin.\n");
+                    printf("Invalid input.Please try again...\n");
                 } else {
                     validInput = 1;
                 }
             }
         }
-        makeMove(&board1,number,direction);
-        printBoard(&board1);
-        printf("%d",checkGame(&board1));
-        //check=checkGame();
+        if(makeMove(&board1,number,direction)==1){ //move to the desired position
+            printf("\n");
+            printBoard(&board1);
+            moves=moves+1;
+        }
     }
+    int score = 1000-10*moves; //calculate score
+    printf("Congratulations! You finished the game.\n");
+    printf("Total number of moves : %d\n",moves);
+    printf("Your Score : %d\n",score);
+    write_best_score(score);//send the score to the write_best_score function and if it is the best score it will write it to the file
 }
 void MainMenu() {
     board board1;
@@ -209,24 +337,27 @@ void MainMenu() {
                 playGameWithUser();
                 break;
             case 2:
-                // Otomatik olarak oyunu bitirme fonksiyonunu çağır
-                // autoFinish();
+                shuffleBoard(&board1);
+                printBoard(&board1);
+                autoFinish(&board1);
+                printf("Total number of computer moves : %d\n",board1.hamle);
+                board1.hamle=0;
                 break;
             case 3:
-                // En iyi skoru gösterme fonksiyonunu çağır
-                // showBestScore();
+                print_best_score();
                 break;
             case 4:
+                printf("Program terminated ...\n");
                 exit(0);
                 break;
             default:
-                printf("Geçersiz bir seçim yaptınız!\n");
+                printf("Invalid choice...\n");
                 break;
         }
     }
 }
 int main() {
-    
+    srand(time(NULL));
    MainMenu();
     
     return 0;
